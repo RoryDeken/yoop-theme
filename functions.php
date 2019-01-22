72cee40ac33446fb2fdc27619eeb7be766b7bba1
Rory Deken <rorydeken@gmail.com>
2019-01-22 12:33:48 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -39,3 +39,49 @@ function tags_categories_support_query($wp_query) {
 // tag and category hooks
 add_action('init', 'tags_categories_support_all');
 add_action('pre_get_posts', 'tags_categories_support_query');
+
+
+
+/**
+* Adds the meta box to the page screen
+*/
+
+
+function intro_text_add_custom_box()
+{
+    $screens = ['post'];
+    foreach ($screens as $screen) {
+        add_meta_box(
+            'intro_text_box_id',           // Unique ID
+            'Post Intro Text',  // Box title
+            'intro_text_box_html',  // Content callback, must be of type callable
+            $screen                   // Post type
+        );
+    }
+}
+add_action('add_meta_boxes', 'intro_text_add_custom_box');
+
+
+function intro_text_box_html($post)
+{
+    $value = get_post_meta($post->ID, '_intro_text_meta_key', true);
+    ?>
+    <label for="intro_text">Intro text that appears before main blog content</label>
+    <textarea name="intro_text" id="intro_text" class="postbox" style="display:block;width:100%;min-height:50px;border:2px solid #e2e4e7;margin-top:10px;padding:10px;border-radius:4px;"><?php echo $value ?></textarea>
+
+    <?php
+	}
+
+
+		function intro_text_save_postdata($post_id)
+	{
+	    if (array_key_exists('intro_text', $_POST)) {
+	        update_post_meta(
+	            $post_id,
+	            '_intro_text_meta_key',
+	            $_POST['intro_text']
+	        );
+	    }
+	}
+	add_action('save_post', 'intro_text_save_postdata');
+
